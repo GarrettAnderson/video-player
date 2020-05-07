dd4830caff7fa8722bebc102798c0da139a1a08e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-05-07 17:39:40 -0400

    added function to control the volume and the speed of the video

--- public/main.js
+++ public/main.js
@@ -28,9 +28,16 @@ function skip() {
   video.currentTime = parseFloat(this.dataset.skip)
 }
 
+function handleRangeUpdate() {
+  console.log(this.value)
+  video.volume = this.value
+  video.playBackRate = this.value
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 toggle.addEventListener('click', togglePlay)
 skipButtons.forEach((button) => button.addEventListener('click', skip))
+ranges.forEach((range) => range.addEventListener('change', handleRangeUpdate))
